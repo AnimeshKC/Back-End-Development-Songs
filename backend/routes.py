c1ce1407bda6e6f9f62ac052ca22cd2a26b442c9
Animesh KC <animesh@ualberta.ca>
2023-09-12 21:27:28 -0400

    health, count, songs

--- backend/routes.py
+++ backend/routes.py
@@ -51,3 +51,18 @@ def parse_json(data):
 ######################################################################
 # INSERT CODE HERE
 ######################################################################
+
+@app.route("/health")
+def health():
+    return jsonify(dict(status="OK")), 200
+
+@app.route("/count")
+def count():
+    """return length of data"""
+    count = db.songs.count_documents({})
+    return {"count": count}, 200
+
+@app.route("/song", methods=["GET"])
+def songs():
+    results = list(db.songs.find({}))
+    return {"songs": parse_json(results)}, 200
